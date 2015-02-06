74652595a82da33bb20543d8a1082a30b56c2a8f
cnichols <mellowdious@mail.com>
2015-02-06 13:59:32 -0800

    Code refactoring

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-var fizzCount = 0;
-var buzzCount = 0;
-var fizzBuzzCount = 0;
+var fizzCount = 0, buzzCount = 0, fizzBuzzCount = 0;
 
 for(i = 1; i <= 100; i++){
-  if(!(i%3) && !(i%5))  { console.log(i, "FizzBuzz"); fizzBuzzCount++; continue; }
-  if(!(i%3))            { console.log(i, "Fizz"); fizzCount++; continue; }
-  if(!(i%5))            { console.log(i, "Buzz"); buzzCount++; continue; }
-  console.log(i);
+  if(!(i%3) && !(i%5)) {
+    console.log(i, "FizzBuzz"); fizzBuzzCount++;
+  }else if(!(i%3)) {
+    console.log(i, "Fizz"); fizzCount++;
+  }else if(!(i%5)) {
+    console.log(i, "Buzz"); buzzCount++;
+  }else{
+    console.log(i);
+  }
 }
 
 console.log("Fizz's:", fizzCount);
